76e2bea81d44ba7423bf5deae46edb756fbf23c4
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-09-19 09:46:07 +0300

    fix: remove escaped newlines from custom lineup regression

--- HattrickAI_V5.OfflineTests/CustomV5LineupRegression.cs
+++ HattrickAI_V5.OfflineTests/CustomV5LineupRegression.cs
@@ -21,7 +21,9 @@ public static class CustomV5LineupRegression
         var custom = new Lineup("Custom XI", "3-2-3", slots);
         var resolvedLeft = RatingPositionResolver.Resolve(custom, "DEF-L");
         var resolvedRight = RatingPositionResolver.Resolve(custom, "DEF-R");
-        var resolvedWbLeft = RatingPositionResolver.Resolve(custom, "WB-L");\n        var resolvedWbRight = RatingPositionResolver.Resolve(custom, "WB-R");\n        if (resolvedWbLeft != RegionalPosition.WingBack || resolvedWbRight != RegionalPosition.WingBack || resolvedLeft != RegionalPosition.CentralDefender || resolvedRight != RegionalPosition.CentralDefender)
+        var resolvedWbLeft = RatingPositionResolver.Resolve(custom, "WB-L");
+        var resolvedWbRight = RatingPositionResolver.Resolve(custom, "WB-R");
+        if (resolvedWbLeft != RegionalPosition.WingBack || resolvedWbRight != RegionalPosition.WingBack || resolvedLeft != RegionalPosition.CentralDefender || resolvedRight != RegionalPosition.CentralDefender)
             return Fail("Legacy DEF-L/DEF-R must resolve as central defenders and WB-L/WB-R as wing-backs.");
 
         var rating = new V5RatingEngine().Calculate(new RatingEngineRequest(custom, players, RatingContext.Default)).Rating;
