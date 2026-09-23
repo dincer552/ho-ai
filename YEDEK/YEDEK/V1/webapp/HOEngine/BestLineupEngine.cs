8cbbc4a50e104c0b2407605ed688fd783f94a93b
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-09-23 06:58:59 +0300

    Add 5-5-0 and 2-5-3 formations

--- YEDEK/YEDEK/V1/webapp/HOEngine/BestLineupEngine.cs
+++ YEDEK/YEDEK/V1/webapp/HOEngine/BestLineupEngine.cs
@@ -14,7 +14,7 @@ public sealed class BestLineupEngine
         new Dictionary<int, PlayerBehaviour>();
 
     public static IReadOnlyList<string> SupportedFormations { get; } =
-        new[] { "4-4-2", "4-3-3", "3-5-2", "4-5-1", "5-4-1", "5-3-2", "3-4-3" };
+        new[] { "4-4-2", "4-3-3", "3-5-2", "4-5-1", "5-4-1", "5-3-2", "3-4-3", "5-5-0", "2-5-3" };
 
     public List<PlayerData> FindBestLineup(List<PlayerData> players, TeamRatings? opponentRatings = null)
     {
